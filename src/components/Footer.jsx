53a3c6e102739a96661a64d59baadc300da76c2b
Shubham Pawar <114252697+shubhampawar4841@users.noreply.github.com>
2025-01-07 22:09:54 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto px-4 text-center">
-  
+ 
       </div>
     </footer>
   )
