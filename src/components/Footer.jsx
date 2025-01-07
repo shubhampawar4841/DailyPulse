215deef71434fa62105ea04832d651289c35a44e
Shubham Pawar <114252697+shubhampawar4841@users.noreply.github.com>
2025-01-07 22:09:34 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto px-4 text-center">
-        <p>&copy; {new Date().getFullYear()} NewsVite. All rights reserved.</p>
+  
       </div>
     </footer>
   )
